d49d498fa7ea3e276feed3b484ea34d0a3787d90
nikola-demo-person <roberto.alsina+nikolademo@gmail.com>
2016-08-29 17:23:05 -0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,16 +17,16 @@ import time
 
 
 # Data about this site
-BLOG_AUTHOR = "Your Name"  # (translatable)
-BLOG_TITLE = "Demo Site"  # (translatable)
+BLOG_AUTHOR = "Roberto Alsina"  # (translatable)
+BLOG_TITLE = "BlogSomeSauce"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://example.com/"
+SITE_URL = "https://blogsomesauce.netlify.com"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
-BLOG_EMAIL = "joe@demo.site"
-BLOG_DESCRIPTION = "This is a demo site for Nikola."  # (translatable)
+BLOG_EMAIL = "info@getnikola.com"
+BLOG_DESCRIPTION = "This is a demo site for Nikola via Netlify & GitHub!"  # (translatable)
 
 # Nikola is multilingual!
 #
