75cbedbc5e69bf0a24ac33ece72e582f23acb54b
Deybi Morales León <morales.economia@gmail.com>
2017-07-27 15:24:06 -0600

    Update conf.py

--- conf.py
+++ conf.py
@@ -180,10 +180,10 @@ POSTS = (
 )
 #quite stories de ntre comillas
 PAGES = (
-    ("pages/*.md", "", "story.tmpl"),
-    ("pages/*.rst", "", "story.tmpl"),
-    ("pages/*.txt", "", "story.tmpl"),
-    ("pages/*.html", "", "story.tmpl"),
+    ("stories/*.md", "", "story.tmpl"),
+    ("stories/*.rst", "", "story.tmpl"),
+    ("stories/*.txt", "", "story.tmpl"),
+    ("stories/*.html", "", "story.tmpl"),
 )
 
 
